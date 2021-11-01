a98ad07c280185765523c5c17952e6291ec61440
Shadi Boomi <shadi.boomi@gmail.com>
2021-11-02 00:42:01 +0100

    added mui + emotion

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,6 +1,10 @@
 import Head from 'next/head';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import * as React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '../mui/theme.js';
 
 export default function Layout({ children }) {
   return (
@@ -10,9 +14,12 @@ export default function Layout({ children }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Navbar />
-      <main>{children}</main>
-      <Footer />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </ThemeProvider>
     </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,30 @@
 import 'tailwindcss/tailwind.css';
-import Layout from '../components/Layout';
+import Layout from '../components/layout/Layout';
+import PropTypes from 'prop-types';
+import createEmotionCache from '../components/mui/createEmotionCache';
+import { CacheProvider } from '@emotion/react';
 
-function MyApp({ Component, pageProps }) {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <CacheProvider value={emotionCache}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CacheProvider>
   );
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
 
 export default MyApp;
