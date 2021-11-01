46889a214ff314cc2262df716a340f7a1c24243e
Shadi Boomi <shadi.boomi@gmail.com>
2021-11-01 17:07:50 +0100

    Deploy Next.js to gh-pages

---
+++ _next/static/cEXVEQb0xTysbgdDUiaBd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-63b973f1e57b3a9f.js"],"/_error":["static/chunks/pages/_error-d828d26f1f5d2954.js"],"/about":["static/chunks/pages/about-319a1a77be79ac09.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
